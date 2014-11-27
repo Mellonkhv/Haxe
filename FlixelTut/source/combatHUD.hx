bf5140f16bc9454039183752969c9f24046e13d3
Nikolay <mellonkhv@gmail.com>
2014-11-28 00:28:16 +1000

    combatHUD часть 2

--- FlixelTut/source/combatHUD.hx
+++ FlixelTut/source/combatHUD.hx
@@ -39,10 +39,26 @@ class combatHUD extends FlxTypedGroup<FlxSprite>
 	private var _enemyHealth:Int;
 	private var _enemyMaxHealth:Int;
 	private var _enemyHealthBar:FlxBar; // FlxBar покажет текущее / максимальное здоровье врага
+	
+	private var _txtPlayerHealth:FlxText // Показывает текущее количество жизни у игрока
+	
+	// Этот массив будет содержать 2 объекта FlxText, которые будут появляться, чтобы показать наносённый уронмили промахи
+	private var _damages:Array<FlxText>;
+	
+	private var _pointer:FlxSprite; // Это будет указатель, чтобы показать, какой вариант (бороться или бежать) выбрал пользователь
+	private var _selected:Int = 0; // Отслеживает какой вариант выбран	
+	private var _choices:Array<FlxText>; //Этот массив будет содержать FlxTexts для наших 2-х вариантов: Борьба и Бегство
+	
+	private var _result:FlxText; // результат битвы
+	
+	private var _alpha:Float = 0; // будет использоваться для быхода из боевого интерфейса
+	private var _wait:Bool = true; // этот флаг будет установлен в верно, когда не хотим, чтобы игрок делал что-нибудь (между витками)
 
-	public function new(MaxSize:Int=0) 
+	
+	
+	public function new() 
 	{
-		super(MaxSize);
+		super();
 		
 	}
 	
